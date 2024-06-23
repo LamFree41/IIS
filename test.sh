5f496d949c45fbccef93d4b45d26480ca7ddd0f0
ajiLam918 <167869006+ajiLam918@users.noreply.github.com>
2024-06-24 06:18:34 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run -d --net=host -it -e NGROK_AUTHTOKEN=2eTzTtisRPj4pnUlPB0p8kGKdMR_5QHLfQkLt8qeaTwFDRnRP ngrok/ngrok:latest tcp --region eu 3389
+docker run -d -it --rm -p 8006:8006 -p 3389:3389 --device=/dev/kvm \
+  -e VERSION='win10' -e RAM_SIZE='32G' -e CPU_CORES='8' \
+  -v /mnt/physics:/storage --cap-add NET_ADMIN --stop-timeout 120 dockurr/windows
+echo "Success!"
